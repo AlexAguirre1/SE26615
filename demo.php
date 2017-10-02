339bd8fdb6425b93fa7b54c2bdf07886d33bbfc1
Alex Aguirre <adaguirre@email.neit.edu>
2017-10-02 19:43:52 -0400

    Hi

--- demo.php
+++ demo.php
@@ -1,7 +1,12 @@
-hi<?php
+hi <?php
 /**
  * Created by PhpStorm.
  * User: Alex
  * Date: 10/2/2017
  * Time: 7:33 PM
  */
+
+$FirstName = "Alex";
+$LastName = "Aguirre";
+
+echo "$FirstName $LastName";
