9fa0bb001a48f20b390ee8a643d8bf1a3ebba05b
Alex Aguirre <adaguirre@email.neit.edu>
2017-10-03 20:30:00 -0400

    table color grid

---
+++ RandomColorGrid.php
@@ -0,0 +1,41 @@
+$table = "<table>";
+    $table = "<table>";
+
+        $table .= "</table>"
+
+
+    function RandomColor()
+    {
+    $color = '#';
+    for ($i = 1; $i <= 7; $i++)
+    {
+    $randNum = rand(0,15);
+    switch ($randNum)
+    {
+    case 10: $randNum = 'A';
+    break;
+    case 11: $randNum = 'B';
+    break;
+    case 12: $randNum = 'C';
+    break;
+    case 13: $randNum = 'D';
+    break;
+    case 14: $randNum = 'E';
+    break;
+    case 15: $randNum = 'F';
+    break;
+    }
+    }
+    }
+    
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
