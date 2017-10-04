753e30b904ab0d3f6056f1d4ec3eb6f249cfe750
Alex Aguirre <adaguirre@email.neit.edu>
2017-10-03 22:56:17 -0400

    adding color to rows/col

--- RandomColorGrid.php
+++ RandomColorGrid.php
@@ -1,32 +1,42 @@
 <?php
 $table = "<table>";
     $table = "<table>";
-
-        $table .= "</table>"
+    for($rows = 0;$rows <= 10;$rows++)
+    {
+     $table.= "\t<tr>";
+        for($col=0; $col <= 10; $col++)
+         {
+            $table .= "<td style='background-color:$color;'>.$color."<br /><span style='color:#ffffff;'>".$color."</span>"</td>;"
+         }
+     $table .="</tr>\n";
+    }
+        $table .= "</table>";
 
 
     function RandomColor()
     {
-    $color = '#';
-    for ($i = 1; $i <= 7; $i++)
-    {
-    $randNum = rand(0,15);
-    switch ($randNum)
-    {
-    case 10: $randNum = 'A';
-    break;
-    case 11: $randNum = 'B';
-    break;
-    case 12: $randNum = 'C';
-    break;
-    case 13: $randNum = 'D';
-    break;
-    case 14: $randNum = 'E';
-    break;
-    case 15: $randNum = 'F';
-    break;
-    }
-    }
+        $color = '#';
+        for ($i = 1; $i <= 7; $i++)
+        {
+        $randNum = rand(0,15);
+        switch ($randNum)
+            {
+            case 10: $randNum = 'A';
+            break;
+            case 11: $randNum = 'B';
+            break;
+            case 12: $randNum = 'C';
+            break;
+            case 13: $randNum = 'D';
+            break;
+            case 14: $randNum = 'E';
+            break;
+            case 15: $randNum = 'F';
+            break;
+            }
+            $color .= $randNum;
+        }
+        return $color;
     }
 
 ?>
@@ -37,6 +47,7 @@ $table = "<table>";
     <title>Title</title>
 </head>
 <body>
+<?php echo $table; ?>
 
 </body>
 </html>
