24489e12ca1db7d7e0694c5384845c8cf52b6375
Aguirre <adaguirre@labs.neit.edu>
2017-10-04 14:30:14 -0400

    Final Grid

--- RandomColorGrid.php
+++ RandomColorGrid.php
@@ -10,7 +10,7 @@ $table = "<table>";
              $g = rand(25,255); // this grabs a random number that is between 128-255 for the RGB, so random number for r then g and finally b.
              $b = rand(25,255);
              $color =  dechex($r) . dechex($g) . dechex($b);// this will allows the random numbers that was generated to be converted from decimal to hexadecimal.
-            $table .= "<td style='background-color: #$color;'>$color<span style='color:#ffffff;'>$color</span></td>"; // this will allow to make the change of the individual box into different colors that was generated. and it will also allow to display the rdg collor code/hexadecimal for the color.
+            $table .= "<td style='background-color: #$color;'>$color<br /><span style='color:#ffffff;'>$color</span></td>"; // this will allow to make the change of the individual box into different colors that was generated. and it will also allow to display the rdg collor code/hexadecimal for the color.
          }
      $table .="</tr>\n";
     }
