5adf3e80843254cb46d8e68b4cfa0c02b4fa1449
AlexAguirre1 <adaguirre@email.neit.edu>
2017-10-16 13:49:47 -0400

    Table/Add dog

--- week3demo/Assets/Dogs.php
+++ week3demo/Assets/Dogs.php
@@ -7,10 +7,31 @@
  */
 function getDogsAsTable($db)
 {
-    $sql = $db->prepare("select * FROM animals");
-    $sql->execute();
-    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-    return $result;
+    try
+    {
+        $sql = $db->prepare("SELECT * FROM animals");
+        $sql->execute();
+        $dogs = $sql->fetchAll(PDO::FETCH_ASSOC);
+        if($sql->rowCount() > 0)
+        {
+            $table = "<table>" . PHP_EOL;
+            foreach($dogs as $dog)
+            {
+                $table .= "<tr><td>" . $dog['name'];
+                $table .= "</td><td>" . $dog['gender'];
+                $table .= "</td><td>" . $dog['fixed'];
+                $table .= "</td></tr>";
+            }
+            $table .= "</table>" . PHP_EOL;
+        } else
+        {
+            $table ="Life is sad. There are no dogs.";
+        }
+        return $table;
+    }catch(PDOException $e)
+    {
+        die("There was a problem retrieving the dogs");
+    }
     /*if (count($result))
     {
         foreach ($result as $dog)
@@ -19,4 +40,19 @@ function getDogsAsTable($db)
         }
     }*/
 
+}
+function  addDog($db, $name, $gender, $fixed)
+{
+    try
+    {
+        $sql = $db->prepare("INSERT INTO animals VALUES (null, :name, :gender, :fixed)");
+        $sql->bindParam(':name', $name);
+        $sql->bindParam(':gender', $gender);
+        $sql->bindParam(':fixed', $fixed);
+        $sql->execute();
+        return $sql->rowCount();
+    }catch(PDOException $e)
+    {
+        die("There was a problem giving birth to the puppy");
+    }
 }
--- week3demo/index.php
+++ week3demo/index.php
@@ -3,6 +3,19 @@ require_once ("Assets/dbconn.php");
 require_once ("Assets/Dogs.php");
 include_once ("Assets/Header.php");
 $db = dbconn();
-var_dump(getDogsAsTable($db));
+$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?? "";
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING) ?? "";
+$gender = filter_input(INPUT_POST, 'gender', FILTER_VALIDATE_REGEXP,
+    array("options"=>array("regexp"=>'/^[MF]$/') )
+) ?? "";
+$fixed = filter_input(INPUT_POST, 'fixed', FILTER_VALIDATE_BOOLEAN) ?? false;
+switch ($action)
+{
+    case "Add":
+        addDog($db, $name, $gender, $fixed);
+        break;
+}
+echo getDogsAsTable($db);
+include_once ("Assets/dogform.php");
 include_once ("Assets/footer.php");
 ?>
