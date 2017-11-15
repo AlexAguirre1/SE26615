51b2ea4dd8432a0645a104b7078fea38c52a4366
AlexAguirre1 <adaguirre@email.neit.edu>
2017-11-15 13:09:48 -0500

    inprogress lab4

--- Lab4/assets/ViewPage.php
+++ Lab4/assets/ViewPage.php
@@ -16,74 +16,79 @@ $id=filter_input(INPUT_POST, 'id',FILTER_SANITIZE_STRING) ?? filter_input(INPUT_
 require_once ("dbconn.php");
 $db = dbconn();
 
-function getCorpsInfoAsTable($db,$id,$cols = null,$sortable = false)
+function getCorpsInfoAsTable($db,$corps,$cols = null,$sortable = false)
+{
+    setlocale(LC_MONETARY, 'en_US.UTF-8');
+    $table = "";
+    if (count($corps) > 0):
+    {
+        $table = "<table>" . PHP_EOL;
+        if ($cols && !$sortable):
+            $table .= "\t<tr>";
+            foreach ($cols as $col) {
+                $table .= "<th>" . $col['id'];
+                $table .= "<th>" . $col['corp'];
+                $table .= "<th>" . $col['incorp_dt'];
+                $table .= "<th>" . $col['email'];// build column headers as anchors, indicating sort=asc&col=colname
+                $table .= "<th>" . $col['Zipcode'];
+                $table .= "<th>" . $col['owner'];
+                $table .= "<th>" . $col['phone'];
+                $table .= "</th>";
+            }
+            $table .= "</tr>" . PHP_EOL;
+        endif;
+        /*if ($cols && $sortable):
+            $dir = "ASC";
+            $table .= "\t<tr>";
+            foreach ($cols as $col) {
+                $table .= "<th>" . $col['id'] . $dir;
+                $table .= "<th>" . $col['corp'] . $dir;
+                $table .= "<th>" . $col['incorp_dt'] .$dir;
+                $table .= "<th>" . $col['email'] . $dir;// build column headers as anchors, indicating sort=asc&col=colname
+                $table .= "<th>" . $col['Zipcode'] .$dir;
+                $table .= "<th>" . $col['owner'] . $dir;
+                $table .= "<th>" . $col['phone'] . $dir;
+                $table.="</th>";
+            }
+            $table .= "</tr>" . PHP_EOL;
+        endif;*/
+
+        foreach ($corps as $corp):
+
+            $table .="\t<tr>";
+            $table .= "<td>" . $corp['corp'];
+            $table .= "<td>" . date('m/d/Y', strtotime($corp['incorp_dt']));// will display the information.
+            $table .= "<td>" . $corp['email'];
+            $table .= "<td>" . $corp['zipcode'];
+            $table .= "<td>" . $corp['owner'];
+            $table .= "<td>" . $corp['phone'];
+            $table .= "<td><form action='#' method='get' ><a href='assets/UpdateCorp.php?id=" . $corp['id'] . "'>Update</a> </form>";
+            $table .= "<td><form action='#' method='get' ><a href='assets/DeleteCorp.php?id=" . $corp['id'] . "'>Delete</a> </form>";//puts a link next to each company name
+            $table .= "<td><form action='#' methods='get'> <a href ='assets/Read.php?id=" . $corp['id'] . "'>Read</a></form>";
+            $table .= "</td>";
+            $table .= "</tr>";
+        endforeach;
+        $table .= "</table>" . PHP_EOL;
+        return $table;
+    //else :
+        //return "<section>We have no employees to display</section>";
+    endif;
+}
+//echo getCorpsInfoAsTable($db,$id);// displays the table.
+function getCorps($db)
 {
     try
     {
-        $sql = $db->prepare("SELECT * FROM corps");//this selects everything from the actor table
-        $sql->bindParam(':id', $id);
-        $sql->execute();
-        $corps = $sql->fetchAll(PDO::FETCH_ASSOC);
-        if($sql->rowCount() > 0)
-        {
-            $table = "<table>" . PHP_EOL;
-            if ($cols && !$sortable):
-                $table .= "\t<tr>";
-                foreach ($cols as $col) {
-                    $table .= "<th>" . $col['id'];
-                    $table .= "<th>" . $col['corp'];
-                    $table .= "<th>" . $col['incorp_dt'];
-                    $table .= "<th>" . $col['email'];// build column headers as anchors, indicating sort=asc&col=colname
-                    $table .= "<th>" . $col['Zipcode'];
-                    $table .= "<th>" . $col['owner'];
-                    $table .= "<th>" . $col['phone'];
-                    $table.="</th>";
-                }
-                $table .= "</tr>" . PHP_EOL;
-            endif;
-            /*if ($cols && $sortable):
-                $dir = "ASC";
-                $table .= "\t<tr>";
-                foreach ($cols as $col) {
-                    $table .= "<th>" . $col['id'] . $dir;
-                    $table .= "<th>" . $col['corp'] . $dir;
-                    $table .= "<th>" . $col['incorp_dt'] .$dir;
-                    $table .= "<th>" . $col['email'] . $dir;// build column headers as anchors, indicating sort=asc&col=colname
-                    $table .= "<th>" . $col['Zipcode'] .$dir;
-                    $table .= "<th>" . $col['owner'] . $dir;
-                    $table .= "<th>" . $col['phone'] . $dir;
-                    $table.="</th>";
-                }
-                $table .= "</tr>" . PHP_EOL;
-            endif;*/
-
-            foreach($corps as $corp)
-            {
-                $table .= "<tr><td><label><b>Company: </b></label>" . $corp['corp'];
-                $table .= "</td><td><label><b>Date: </b></label>" . date('m/d/Y', strtotime($corp['incorp_dt']));// will display the information.
-                $table .= "</td><td><label><b>Email: </b></label>" . $corp['email'];
-                $table .= "</td><td><label><b>Zip Code: </b></label>" . $corp['zipcode'];
-                $table .= "</td><td><label><b>Owner: </b></label>" . $corp['owner'];
-                $table .= "</td><td><label><b>Phone: </b></label>" . $corp['phone'];
-                $table .= "</td><td><form action='#' method='get' ><a href='assets/UpdateCorp.php?id=" . $corp['id'] ."'>Update</a> </form>";
-                $table .="</td><td><form action='#' method='get' ><a href='assets/DeleteCorp.php?id=". $corp['id'] ."'>Delete</a> </form>";//puts a link next to each company name
-                $table .= "</td><td><form action='#' methods='get'> <a href ='assets/Read.php?id=". $corp['id'] ."'>Read</a></form>";
-                $table .= "</td></tr>";
-            }
-            $table .= "</table>" . PHP_EOL;
-
-        } else
-        {
-            $table ="Nothing Found.";
-        }
-        return $table;
-    }catch(PDOException $e)
+        $sql = "SELECT `corps`.`id`, `corps`.`corp`, `corps`.`incorp_dt`, `corps`.`email`, `corps`.`zipcode`, `corps`.`owner`, `corps`.`phone` FROM `corps` WHERE id=:id";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $corps = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }catch (PDOException $e)
     {
         die($e);
     }
-
+    return $corps;
 }
-echo getCorpsInfoAsTable($db,$id);// displays the table.
 
 function getCorpsAsSortedTable($db, $col, $dir) {
     try {
