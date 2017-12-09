53d050bd2041a2a2497f39e3c4274d18eb4ec8d0
Alex Aguirre <adaguirre@email.neit.edu>
2017-12-08 20:29:30 -0500

    checkpoint

--- ShoppingCart/Assets/AdminPage.php
+++ ShoppingCart/Assets/AdminPage.php
@@ -5,10 +5,37 @@
  * Date: 12/7/2017
  * Time: 8:03 PM
  */
+require_once ("dbconn.php");
+include_once ("Functions.php");
+$db=dbconn();
+$action= filter_input(INPUT_POST, 'action',FILTER_SANITIZE_STRING) ?? filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?? NULL;
+$Cate= filter_input(INPUT_POST, 'CName',FILTER_SANITIZE_STRING) ?? "";
+$id =filter_input(INPUT_POST, 'id',FILTER_SANITIZE_STRING) ?? filter_input(INPUT_GET, 'id',FILTER_SANITIZE_STRING) ?? "";
+
 session_start();
 if(!isset($_SESSION['email']) || empty($_SESSION['email'])){
     header("location: ..\index.php");
     exit;
+}
+
+
+switch($action)
+{
+    case "Add":
+        AddCategories($db,$Cate);
+        break;
+    case"Edit":
+        break;
+    case"Delete":
+        DeleteCategories($db, $id);
+        break;
+    case"List":
+        break;
+    case"Choose Image":
+        break;
+    case"Add Product":
+        break;
+
 }
 //echo "Welcome to the Admin Page, coming Soon.";
 /*session_start();
@@ -24,26 +51,32 @@ $db->close();*/
 <style>
 <?php include("stylesheet.css") ?>
 </style>
-<!--<h2>Welcome - <?php echo $rows['email']; ?></h2>-->
 <header>
     <h2>Welcome to the Admin Page!</h2>
     <a href="Logout.php?logout">Logout</a>
     </header>
 
 <!--<a href="..\index.php">Home</a>-->
+<form method="post" action="#">
 <!-- drop Down List should go right here-->
-<input type="submit" name="Edit" value="Edit" />
-<input type="submit" name="Delete" value="Delete" />
-<input type="submit" name="List" value="List" />
+<?php echo getCategories($db) ?>
+<input type="submit" name="action" value="Edit" />
+<input type="submit" name="action" value="Delete" />
+<input type="submit" name="action" value="List" />
+</form>
+<form method="post" action="#">
 <div class="manage">
 <label>Category Name</label>: <input type="text" name="CName" value='' required="" class="textBox"/><br />
-    <input type="submit" name="AddC" value="Add Category" />
+    <input type="submit" name="action" value="Add" />
 </div>
+</form>
 <br />
+<form method="post" action="#">
 <label>Product Name</label>: <input type="text" name="PName" value='' required="" class="textBox"/><br />
 <!-- Drop Down list for Products should go right-->
 <div class="manage">
 <label>Price $</label>: <input type="text" name="Price" value='' required="" class="textBox"/><br />
-    <input type="submit" name="Images" value="Choose Image" />
-    <input type="submit" name="Add" value="Add Product" />
+    <input type="submit" name="action" value="Choose Image" />
+    <input type="submit" name="action" value="Add Product" />
 </div>
+</form>
--- ShoppingCart/Assets/Functions.php
+++ ShoppingCart/Assets/Functions.php
@@ -7,14 +7,50 @@
  */
 require_once ("dbconn.php");
 $db=dbconn();
-function getCategories()
+$id =filter_input(INPUT_POST, 'id',FILTER_SANITIZE_STRING) ?? filter_input(INPUT_GET, 'id',FILTER_SANITIZE_STRING) ?? "";
+function getCategories($db)
 {
-    
+    try
+    {
+        $sql = $db->prepare("SELECT * FROM categories WHERE 0=0");
+        $sql->execute();
+        $category = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        if($sql->rowCount() > 0)
+        {
+            $dropDown = "<select name='list'>" . PHP_EOL;
+            foreach ($category as $Cate)
+            {
+                $dropDown .="<option value='" . $Cate['category_id'] . "'>". $Cate['category']."</option>";
+            }
+            $dropDown .="</select>";
+        }
+        else
+        {
+            $dropDown = "There was nothing found" . PHP_EOL;
+        }
+        return $dropDown;
+    }
+    catch(PDOException $e)
+    {
+        die($e);
+    }
+
 }
 
-function AddCategories()
+function AddCategories($db,$Cate)
 {
+    try
+    {
+        $sql = $db->prepare("INSERT INTO categories VALUES (null,:category)");//inserts/add a ne actore each time to form is filled out.
+        $sql->bindParam(':category', $Cate);
+        $sql->execute();
+        echo($sql->rowCount(). "rows inserted.");
 
+    }catch(PDOException $e)
+    {
+        die($e);//will let me know if there is any errors specifically.
+    }
 }
 
 function EditCategories()
@@ -22,7 +58,18 @@ function EditCategories()
 
 }
 
-function DeleteCategories()
+function DeleteCategories($db, $id)
 {
+    try
+    {
+        $sql = $db->prepare("DELETE FROM categories WHERE category_id =:id");//this will delete everything that the id pulls.
+        $sql->bindParam(':id', $id);
+        $sql->execute();
+        return $sql->rowCount(). "rows deleted";
 
+    }catch(PDOException $e)
+    {
+        die("There was an issue deleting the company");
+    }
 }
+//echo(DeleteCategories($db,$id));
